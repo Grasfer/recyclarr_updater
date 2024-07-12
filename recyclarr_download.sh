e6358b309afbd10ccee0c30dd4746ab05c93a154
Grasfer <grasfer@gmail.com>
2024-07-12 09:37:54 +0200

    Update recyclarr_download.sh

--- recyclarr_download.sh
+++ recyclarr_download.sh
@@ -4,8 +4,8 @@ today=$(date +"%Y-%m-%d")
 
 RECYCLARR_URL="https://github.com/recyclarr/recyclarr/releases/latest/download/recyclarr-linux-x64.tar.xz"
 
-wget "$RECYCLARR_URL" -O - | sudo tar xJ --overwrite -C /usr/local/bin
+sudo wget "$RECYCLARR_URL" -O - | sudo tar xJ --overwrite -C /usr/local/bin
 
 logger Recyclarr updated.
 
-echo "Recyclarr updated ${today}." >> /var/log/recyclarr.log
+echo "Recyclarr updated ${today}." >> ./recyclarr-download.log
